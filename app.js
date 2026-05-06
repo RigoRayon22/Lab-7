ab96cfc3eec24cb672e93784574997d9a0d18626
RigoRayon22 <rrayon@csumb.edu>
2026-05-05 19:45:18 -0700

    fix port for render

--- app.js
+++ app.js
@@ -257,6 +257,8 @@ app.get("/deleteQuote", isAuthenticated, async (req, res) => {
   res.redirect("/quotes");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
